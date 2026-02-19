06a8e14d2105c3e01a4b0343a6fc15cbbd43cf29
Gregory Ogbili <gregoryogbili@gmail.com>
2026-02-19 02:39:31 +0000

    add canonical redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.newstrac.org",
+          },
+        ],
+        destination: "https://newstrac.org/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
