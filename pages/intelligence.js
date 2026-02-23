052e9f0b1f9390831d15a8f6cc6845e070ab1b6d
Gregory Ogbili <gregoryogbili@gmail.com>
2026-02-23 19:32:23 +0000

    Fix missing default exports for intelligence and regions

--- pages/intelligence.js
+++ pages/intelligence.js
@@ -1,1 +1,24 @@
 import TopNav from "../components/TopNav";
+import Image from "next/image";
+
+export default function Intelligence() {
+  return (
+    <>
+      <TopNav
+        active="/intelligence"
+        logoImg={
+          <Image
+            src="/logo.png"
+            alt="NewsTrac Logo"
+            width={34}
+            height={34}
+          />
+        }
+      />
+
+      <div style={{ padding: 30 }}>
+        Intelligence Page
+      </div>
+    </>
+  );
+}
--- pages/regions.js
+++ pages/regions.js
@@ -1,1 +1,24 @@
 import TopNav from "../components/TopNav";
+import Image from "next/image";
+
+export default function Regions() {
+  return (
+    <>
+      <TopNav
+        active="/regions"
+        logoImg={
+          <Image
+            src="/logo.png"
+            alt="NewsTrac Logo"
+            width={34}
+            height={34}
+          />
+        }
+      />
+
+      <div style={{ padding: 30 }}>
+        Regions Page
+      </div>
+    </>
+  );
+}
