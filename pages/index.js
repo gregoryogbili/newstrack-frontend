c22acab7bc0e7fee075c7b194a0280f6d6e013fa
Gregory Ogbili <gregory.ogbili@gmail.com>
2026-03-31 20:58:56 +0100

    logo size

--- pages/index.js
+++ pages/index.js
@@ -269,8 +269,8 @@ export default function Home() {
           <Image
             src="/logo.png"
             alt="NewsTrac Logo"
-            width={34}
-            height={34}
+            width={40}
+            height={40}
             priority
           />
         }
